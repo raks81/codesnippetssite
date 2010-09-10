91d7b1d01edc18bf54f299179f1b4e9ed45daebe
rakzrao81@gmail.com <rakzrao81@gmail.com@4dd0689e-4c6d-11de-9e51-a92ed582648b>
2010-09-10 05:55:31 +0000

    Update

--- trunk/AppDeployerCore/src/com/rr/appdeployer/ProcessRunner.java
+++ trunk/AppDeployerCore/src/com/rr/appdeployer/ProcessRunner.java
@@ -5,24 +5,24 @@
 package com.rr.appdeployer;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author raks
  */
-public class ProcessRunner { 
+public class ProcessRunner {
 
-    public static String executeProcess(String path, StringBuffer output) {
+    public static String executeProcess(String path) {
+        StringBuffer output = new StringBuffer();
         try {
-            String line;
+
             Process p = Runtime.getRuntime().exec(path);
-            BufferedReader input =
-                    new BufferedReader(new InputStreamReader(p.getInputStream()));
-            while ((line = input.readLine()) != null ) {
-                output.append(line + "\n");
-            }
-            input.close();
+            Thread t = new Thread(new ProcessOutputReader(p, output));
+            t.start();
             p.waitFor();
             if (p.exitValue() != 0) {
                 throw new RuntimeException("Process execution failed. Process returned exit value: " + p.exitValue() + " \n The standard output is\n" + output.toString());
@@ -31,5 +31,30 @@ public class ProcessRunner {
             err.printStackTrace();
         }
         return output.toString();
+    }
+}
+
+class ProcessOutputReader implements Runnable {
+
+    Process p;
+    StringBuffer output;
+
+    public ProcessOutputReader(Process p, StringBuffer output) {
+        this.p = p;
+        this.output = output;
+    }
+
+    public void run() {
+        try {
+            String line;
+            BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            while ((line = input.readLine()) != null) {
+                output.append(line + "\n");
+            }
+            input.close();
+        } catch (IOException ex) {
+            Logger.getLogger(ProcessOutputReader.class.getName()).log(Level.SEVERE, "Error while reading output of the process!! ", ex);
+        }
+
     }
 }
