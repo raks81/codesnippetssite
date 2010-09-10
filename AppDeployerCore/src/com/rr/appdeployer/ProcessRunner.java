6a51acce7245f78e2db9c22a346386e35af2ecd0
rakzrao81@gmail.com <rakzrao81@gmail.com@4dd0689e-4c6d-11de-9e51-a92ed582648b>
2010-09-10 03:47:11 +0000

    init

---
+++ AppDeployerCore/src/com/rr/appdeployer/ProcessRunner.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rr.appdeployer;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author raks
+ */
+public class ProcessRunner {
+
+    public static String executeProcess(String path) {
+        StringBuffer output = new StringBuffer();
+        try {
+            String line;
+            Process p = Runtime.getRuntime().exec(path);
+            BufferedReader input =
+                    new BufferedReader(new InputStreamReader(p.getInputStream()));
+            while ((line = input.readLine()) != null) {
+                output.append(line + "\n");
+            }
+            input.close();
+            if (p.exitValue() != 0) {
+                throw new RuntimeException("Process execution failed. Process returned exit value: " + p.exitValue() + " \n The standard output is\n" + output.toString());
+            }
+        } catch (Exception err) {
+            err.printStackTrace();
+        }
+        return output.toString();
+    }
+}
