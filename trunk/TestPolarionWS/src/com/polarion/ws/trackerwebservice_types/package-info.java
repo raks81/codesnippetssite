2b2d8f5a211a9a7912812a292da719d7ba799660
rakzrao81@gmail.com <rakzrao81@gmail.com@4dd0689e-4c6d-11de-9e51-a92ed582648b>
2012-04-29 04:20:24 +0000

    cleanup

--- trunk/TestPolarionWS/src/com/polarion/ws/trackerwebservice_types/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://ws.polarion.com/TrackerWebService-types", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.polarion.ws.trackerwebservice_types;
--- trunk/TestPolarionWS/src/com/polarion/ws/types/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://ws.polarion.com/types", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.polarion.ws.types;
