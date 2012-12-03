f6180b69fd62d87a11f39cd5ae63a3fbbdcb535e
rakzrao81@gmail.com <rakzrao81@gmail.com@4dd0689e-4c6d-11de-9e51-a92ed582648b>
2012-12-03 15:40:05 +0000

    Some updates

--- trunk/syncDocs/src/net/rr/syncdocs/AuthServlet.java
+++ trunk/syncDocs/src/net/rr/syncdocs/AuthServlet.java
@@ -4,10 +4,10 @@ import java.io.IOException;
 import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
-public class SyncDocsServlet extends HttpServlet {
+public class AuthServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		resp.getWriter().println("Hello, Auth world");
 	}
 }
