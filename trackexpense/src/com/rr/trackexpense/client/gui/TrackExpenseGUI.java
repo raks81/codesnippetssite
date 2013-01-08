9a344ca810a015d42b58d5fbf45bad711d52254d
rakzrao81@gmail.com <rakzrao81@gmail.com@4dd0689e-4c6d-11de-9e51-a92ed582648b>
2013-01-08 16:54:18 +0000

    added monthly grouping feature

--- trackexpense/src/com/rr/trackexpense/client/gui/TrackExpenseGUI.java
+++ trackexpense/src/com/rr/trackexpense/client/gui/TrackExpenseGUI.java
@@ -1,8 +1,13 @@
 package com.rr.trackexpense.client.gui;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import com.google.gwt.event.dom.client.ChangeEvent;
+import com.google.gwt.event.dom.client.ChangeHandler;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.i18n.client.DateTimeFormat;
@@ -10,6 +15,7 @@ import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Grid;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.datepicker.client.CalendarUtil;
 import com.rr.trackexpense.client.service.TrackExpenseClientImpl;
@@ -24,16 +30,32 @@ public class TrackExpenseGUI extends Composite {
 	private Grid grid = new Grid();
 	private TrackExpenseClientImpl serviceImpl;
 	private Label totalLbl = new Label();
+	private ListBox monthSelector = new ListBox();
 
 	/**
 	 * @param serviceImpl
 	 */
-	public TrackExpenseGUI(TrackExpenseClientImpl serviceImpl) {
+	public TrackExpenseGUI(final TrackExpenseClientImpl serviceImpl) {
 		this.serviceImpl = serviceImpl;
 		initWidget(vPanel);
 		Button btn1 = new Button("Get Expenses");
 		btn1.addClickHandler(new Btn1ClickHandler());
+		vPanel.add(monthSelector);
+		// Add a handler to handle drop box events
+		monthSelector.addChangeHandler(new ChangeHandler() {
+			@Override
+			public void onChange(ChangeEvent event) {
+				if (vPanel.getWidgetIndex(grid) != -1) {
+					vPanel.remove(grid);
+				}
+				serviceImpl.getExpenses();
+			}
+		});
+
+		vPanel.add(grid);
+		vPanel.add(totalLbl);
 		vPanel.add(btn1);
+		serviceImpl.getExpenses();
 	}
 
 	private class Btn1ClickHandler implements ClickHandler {
@@ -51,33 +73,50 @@ public class TrackExpenseGUI extends Composite {
 
 	public void updateTable(List<Expense> expenses) {
 		if (expenses != null && expenses.size() > 0) {
-			// Create a grid
-			grid = new Grid(expenses.size(), 4);
 
-			// Add images to the grid
-			Double total = 0d;
-			int numRows = grid.getRowCount();
-			int row = 0;
-			for (; row < numRows; row++) {
-				total += expenses.get(row).getAmount().doubleValue();
-				Date spentdate = expenses.get(row).getSpentDate();
-				grid.setWidget(row, 0, new Label(DateTimeFormat
-						.getShortDateFormat().format(spentdate)));
-				if (CalendarUtil.getDaysBetween(new Date(), spentdate) == 0) {
-					grid.getWidget(row, 0).setStyleName("bold");
+			Map<String, List<Expense>> groupedExpenses = getMonthlyGroupedExpenses(expenses);
+
+			if (monthSelector.getItemCount() == 0) {
+				for (String month : groupedExpenses.keySet()) {
+					monthSelector.addItem(month);
 				}
-				grid.setWidget(row, 1,
-						new Label(expenses.get(row).getSpentOn()));
-				grid.setWidget(row, 2, new Label(expenses.get(row).getAmount()
-						.toString()));
-				Button deleteBtn = new Button("X");
-				deleteBtn.addClickHandler(new DeleteExpenseClickHandler(
-						expenses.get(row), row));
-				grid.setWidget(row, 3, deleteBtn);
 			}
-			vPanel.add(grid);
-			totalLbl = new Label("Total: " + total.toString());
-			vPanel.add(totalLbl);
+
+			if (monthSelector.getSelectedIndex() >= 0) {
+				List<Expense> monthlyExpense = groupedExpenses
+						.get(monthSelector.getValue(monthSelector
+								.getSelectedIndex()));
+				if (monthlyExpense != null) {
+					// Create a grid
+					grid = new Grid(monthlyExpense.size(), 4);
+
+					Double total = 0d;
+					int numRows = monthlyExpense.size();
+					int row = 0;
+					for (; row < numRows; row++) {
+						total += monthlyExpense.get(row).getAmount()
+								.doubleValue();
+						Date spentdate = monthlyExpense.get(row).getSpentDate();
+						grid.setWidget(row, 0, new Label(DateTimeFormat
+								.getShortDateFormat().format(spentdate)));
+						if (CalendarUtil.getDaysBetween(new Date(), spentdate) == 0) {
+							grid.getWidget(row, 0).setStyleName("bold");
+						}
+						grid.setWidget(row, 1, new Label(monthlyExpense
+								.get(row).getSpentOn()));
+						grid.setWidget(row, 2, new Label(monthlyExpense
+								.get(row).getAmount().toString()));
+						Button deleteBtn = new Button("X");
+						deleteBtn
+								.addClickHandler(new DeleteExpenseClickHandler(
+										monthlyExpense.get(row), row));
+						grid.setWidget(row, 3, deleteBtn);
+					}
+					if (vPanel.getWidgetIndex(grid) < 0)
+						vPanel.add(grid);
+					totalLbl = new Label("Total: " + total.toString());
+				}
+			}
 		}
 	}
 
@@ -95,5 +134,20 @@ public class TrackExpenseGUI extends Composite {
 			grid.removeRow(row);
 			serviceImpl.deleteExpense(expense);
 		}
+	}
+
+	private Map<String, List<Expense>> getMonthlyGroupedExpenses(
+			List<Expense> allExpenses) {
+		if (allExpenses == null)
+			return null;
+		Map<String, List<Expense>> grpdExpenses = new HashMap<String, List<Expense>>();
+		for (Expense expense : allExpenses) {
+			String month = DateTimeFormat.getFormat("MM-yyyy (MMM)").format(
+					expense.getSpentDate());
+			if (grpdExpenses.get(month) == null)
+				grpdExpenses.put(month, new ArrayList<Expense>());
+			grpdExpenses.get(month).add(expense);
+		}
+		return grpdExpenses;
 	}
 }
