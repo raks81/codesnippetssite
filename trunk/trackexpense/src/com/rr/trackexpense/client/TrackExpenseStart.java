880647e815a3401e2c26e2fcecf10907b53681e5
rakzrao81@gmail.com <rakzrao81@gmail.com@4dd0689e-4c6d-11de-9e51-a92ed582648b>
2013-01-13 16:43:42 +0000

    Added login Fixed multiple expenses Upgraded app engine

--- trunk/trackexpense/src/com/rr/trackexpense/client/TrackExpenseStart.java
+++ trunk/trackexpense/src/com/rr/trackexpense/client/TrackExpenseStart.java
@@ -2,24 +2,59 @@ package com.rr.trackexpense.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Anchor;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.rr.trackexpense.client.gui.TrackExpenseGUI;
+import com.google.gwt.user.client.ui.VerticalPanel;
 import com.rr.trackexpense.client.service.TrackExpenseClientImpl;
+import com.rr.trackexpense.shared.model.LoginInfo;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class TrackExpenseStart implements EntryPoint {
 
-	private TrackExpenseClientImpl serviceImple;
+	private LoginInfo loginInfo = null;
+	private VerticalPanel loginPanel = new VerticalPanel();
+	private Label loginLabel = new Label(
+			"Please sign in to your Google Account.");
+	private Anchor signInLink = new Anchor("Sign In");
 
 	/**
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
+		// Check login status using login service.
+		LoginServiceAsync loginService = GWT.create(LoginService.class);
+		loginService.login(GWT.getHostPageBaseURL(),
+				new AsyncCallback<LoginInfo>() {
+					public void onFailure(Throwable error) {
+					}
+
+					public void onSuccess(LoginInfo result) {
+						loginInfo = result;
+						if (loginInfo.isLoggedIn()) {
+							loadGUI();
+						} else {
+							loadLogin();
+						}
+					}
+				});
+	}
+
+	private void loadGUI() {
 		TrackExpenseClientImpl clientImpl = new TrackExpenseClientImpl(
 				GWT.getModuleBaseURL() + "trackexpenseservice");
 		RootPanel.get().add(clientImpl.getMaingui());
+	}
+
+	private void loadLogin() {
+		// Assemble login panel.
+		signInLink.setHref(loginInfo.getLoginUrl());
+		loginPanel.add(loginLabel);
+		loginPanel.add(signInLink);
+		RootPanel.get().add(loginPanel);
 	}
 
 }
