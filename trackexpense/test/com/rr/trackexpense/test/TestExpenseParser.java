7a4c412195041faa7a1ac0d7ed58e1a28adbfb48
rakzrao81@gmail.com <rakzrao81@gmail.com@4dd0689e-4c6d-11de-9e51-a92ed582648b>
2013-01-25 12:41:47 +0000

    Support -ve amounts

--- trackexpense/test/com/rr/trackexpense/test/TestExpenseParser.java
+++ trackexpense/test/com/rr/trackexpense/test/TestExpenseParser.java
@@ -85,6 +85,53 @@ public class TestExpenseParser {
 				.compareTo(new BigDecimal("10.34")) == 0);
 	}
 	
+	@Test
+	public void testNegetiveAmount() {
+		String expString = "Kadlekai- -400";
+		Expense expense = TrackExpenseUtils.convertToExpense(expString);
+		Assert.assertTrue(expense != null);
+		Assert.assertTrue(expense.getSpentOn() != null);
+		Assert.assertTrue(expense.getAmount() != null);
+		Assert.assertTrue(expense.getSpentDate() != null);
+		Assert.assertTrue(expense.getSpentOn().equals("Kadlekai"));
+		// Assert.assertTrue(expense.getSpentDate().before(
+		// Calendar.getInstance().getTime()));
+		Assert.assertTrue(expense.getAmount()
+				.compareTo(new BigDecimal("-400")) == 0);
+	}
+	
+	@Test
+	public void testNegetiveAmountWithoutSpace() {
+		String expString = "Kadlekai--400";
+		Expense expense = TrackExpenseUtils.convertToExpense(expString);
+		Assert.assertTrue(expense != null);
+		Assert.assertTrue(expense.getSpentOn() != null);
+		Assert.assertTrue(expense.getAmount() != null);
+		Assert.assertTrue(expense.getSpentDate() != null);
+		Assert.assertTrue(expense.getSpentOn().equals("Kadlekai"));
+		// Assert.assertTrue(expense.getSpentDate().before(
+		// Calendar.getInstance().getTime()));
+		Assert.assertTrue(expense.getAmount()
+				.compareTo(new BigDecimal("-400")) == 0);
+	}
+	
+	
+	
+	@Test
+	public void testNegetiveDecimal() {
+		String expString = "Kadlekai- -10.34";
+		Expense expense = TrackExpenseUtils.convertToExpense(expString);
+		Assert.assertTrue(expense != null);
+		Assert.assertTrue(expense.getSpentOn() != null);
+		Assert.assertTrue(expense.getAmount() != null);
+		Assert.assertTrue(expense.getSpentDate() != null);
+		Assert.assertTrue(expense.getSpentOn().equals("Kadlekai"));
+		// Assert.assertTrue(expense.getSpentDate().before(
+		// Calendar.getInstance().getTime()));
+		Assert.assertTrue(expense.getAmount()
+				.compareTo(new BigDecimal("-10.34")) == 0);
+	}
+	
 	
 	
 }
